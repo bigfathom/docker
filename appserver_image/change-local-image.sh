f7804574a4c41cde91cb2765465d24ecdd242bc3
Frank Font <mrfont@room4me.com>
2018-04-14 14:09:43 -0400

    rebuilt with latest application code

---
+++ appserver_image/change-local-image.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo Starting $0
+source mysettings.env
+
+CURRENT_TAG_NAME=$1
+NEW_TAG_NAME=$2
+
+function showUsage()
+{
+    echo "PURPOSE: Save a LOCAL image to your docker image collection with a new TAG"
+    echo "         and with the LOCAL designation removed."
+    echo
+    echo "USAGE: $0 CURRENT_TAG_NAME NEW_TAG_NAME"
+    echo
+    echo Current bigfathom images ...
+    docker image ls | grep "bigfathom"
+    echo
+}
+
+if [ -z "$CURRENT_TAG_NAME" ]; then
+    echo "ERROR missing CURRENT_TAG_NAME"
+    showUsage
+    exit 2
+fi
+if [ -z "$NEW_TAG_NAME" ]; then
+    echo "ERROR missing NEW_TAG_NAME"
+    showUsage
+    exit 2
+fi
+if [ "$CURRENT_TAG_NAME" = "$NEW_TAG_NAME" ]; then
+    echo "ERROR CURRENT_TAG_NAME same as NEW_TAG_NAME"
+    showUsage
+    exit 2
+fi
+
+echo "... IMAGE NAME = ${IMAGE_NAME}"
+echo "... CURRENT TAG NAME   = ${CURRENT_TAG_NAME}"
+echo "... NEW TAG NAME   = ${NEW_TAG_NAME}"
+
+CMD_TAG="docker tag local/${IMAGE_NAME}:${CURRENT_TAG_NAME} ${IMAGE_NAME}:${NEW_TAG_NAME}"
+echo $CMD_TAG
+eval $CMD_TAG
+echo
+
+echo Finished $0
+echo
+
--- appserver_image/share-image.sh
+++ appserver_image/share-image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo Starting $0
+VERSIONINFO=20180414.1
+echo "Starting $0 v$VERSIONINFO"
 source mysettings.env
 
 TAG_NAME=$1
@@ -8,20 +9,45 @@ if [ -z "$TAG_NAME" ]; then
     TAG_NAME="latest"
 fi
 
-echo "... IMAGE NAME = ${IMAGE_NAME}"
-echo "... TAG NAME   = ${TAG_NAME}"
+IMAGE_PREFIX=$2
+if [ -z "$TAG_NAME" ]; then
+    IMAGE_PREFIX=""
+else
+    IMAGE_PREFIX="${IMAGE_PREFIX}/"
+fi
 
+SRC_IMAGE_NAME="${IMAGE_PREFIX}${IMAGE_NAME}"
+
+if [ ! -z "$IMAGE_PREFIX" ]; then
+  echo "Create NON '$IMAGE_PREFIX' PREFIXED image image for us to push..."
+  CMD_TAG="docker tag ${SRC_IMAGE_NAME}:${TAG_NAME} ${IMAGE_NAME}:${TAG_NAME}"
+  echo $CMD_TAG
+  eval $CMD_TAG
+  echo
+fi
+
+echo
+echo "CURRENT IMAGES..."
+docker image ls | grep "${IMAGE_NAME}" | grep "${TAG_NAME}"
+echo
+
+echo
+echo "... SOURCE IMAGE NAME = ${SRC_IMAGE_NAME}"
+echo "... TAG NAME          = ${TAG_NAME}"
+echo
+
+echo "Login to docker..."
 CMD_LOGIN="docker login"
 echo $CMD_LOGIN
 eval $CMD_LOGIN
 echo
 
-CMD_TAG="docker tag local/${IMAGE_NAME}:${TAG_NAME} ${IMAGE_NAME}:${TAG_NAME}"
-echo $CMD_TAG
-eval $CMD_TAG
-echo
-
+echo "Share the image..."
 CMD_PUSH="docker push ${IMAGE_NAME}:${TAG_NAME}"
 echo $CMD_PUSH
 eval $CMD_PUSH
 echo
+
+echo "Finished $0 v$VERSIONINFO"
+echo
+
